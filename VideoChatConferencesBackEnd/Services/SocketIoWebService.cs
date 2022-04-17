eaf7368acae7e8de0f895e036ad8ec20de8b8ffb
tevkr <43213523+tevkr@users.noreply.github.com>
2022-04-17 19:44:52 +0300

    Added hash for rest api

--- VideoChatConferencesBackEnd/Services/SocketIoWebService.cs
+++ VideoChatConferencesBackEnd/Services/SocketIoWebService.cs
@@ -12,6 +12,7 @@ namespace VideoChatConferencesBackEnd.Services
         }
         public static async Task AddRoomAsync(string name, string id, string password)
         {
+            var hashVal = StringToMD5(id + "nom_xd_prod");
             var values = new Dictionary<string, string?>
             {
                 { "name", name },
@@ -21,23 +22,19 @@ namespace VideoChatConferencesBackEnd.Services
                 { "usersCount", "0" }
             };
             var content = new FormUrlEncodedContent(values);
-            var response = await Client.PostAsync($"{Url}add-room", content);
+            var response = await Client.PostAsync($"{Url}add-room?hashVal={hashVal}", content);
             var responseString = await response.Content.ReadAsStringAsync();
-        }
-        public static async Task<bool> HasOwner(string roomId)
-        {
-            var responseString = await Client.GetStringAsync($"{Url}has-owner?roomId={roomId}");
-            return Newtonsoft.Json.JsonConvert.DeserializeObject<bool>(responseString);
         }
         public static async Task<bool> SetOwnerIfNotExists(string roomId, string ownerId)
         {
+            var hashVal = StringToMD5(roomId + "nom_xd_prod");
             var values = new Dictionary<string, string?>
             {
                 { "roomId", roomId },
                 { "ownerId", ownerId }
             };
             var content = new FormUrlEncodedContent(values);
-            var response = await Client.PostAsync($"{Url}set-owner-if-not-exists", content);
+            var response = await Client.PostAsync($"{Url}set-owner-if-not-exists?hashVal={hashVal}", content);
             var responseString = await response.Content.ReadAsStringAsync();
             if (responseString == "\"success\"")
                 return true;
@@ -46,23 +43,35 @@ namespace VideoChatConferencesBackEnd.Services
         }
         public static async Task<bool> IsPasswordCorrect(string roomId, string password)
         {
-            var responseString = await Client.GetStringAsync($"{Url}is-password-correct?roomId={roomId}&password={password}");
+            var hashVal = StringToMD5(roomId + "nom_xd_prod");
+            var responseString = await Client.GetStringAsync($"{Url}is-password-correct?hashVal={hashVal}&roomId={roomId}&password={password}");
             return Newtonsoft.Json.JsonConvert.DeserializeObject<bool>(responseString);
         }
         public static async Task<bool> IsOwner(string roomId, string userId)
         {
-            var responseString = await Client.GetStringAsync($"{Url}is-owner?roomId={roomId}&userId={userId}");
+            var hashVal = StringToMD5(roomId + "nom_xd_prod");
+            var responseString = await Client.GetStringAsync($"{Url}is-owner?hashVal={hashVal}&roomId={roomId}&userId={userId}");
             return Newtonsoft.Json.JsonConvert.DeserializeObject<bool>(responseString);
         }
         public static async Task<bool> IsRoomExists(string roomId)
         {
-            var responseString = await Client.GetStringAsync($"{Url}is-room-exists?roomId={roomId}");
+            var hashVal = StringToMD5(roomId + "nom_xd_prod");
+            var responseString = await Client.GetStringAsync($"{Url}is-room-exists?hashVal={hashVal}&roomId={roomId}");
             return Newtonsoft.Json.JsonConvert.DeserializeObject<bool>(responseString);
         }
         public static async Task<List<RoomModel>?> GetAllRooms()
         {
             var responseString = await Client.GetStringAsync($"{Url}get-all-rooms");
             return Newtonsoft.Json.JsonConvert.DeserializeObject<List<RoomModel>?>(responseString);
+        }
+        private static string StringToMD5(string input)
+        {
+            using (System.Security.Cryptography.MD5 md5 = System.Security.Cryptography.MD5.Create())
+            {
+                byte[] inputBytes = System.Text.Encoding.ASCII.GetBytes(input);
+                byte[] hashBytes = md5.ComputeHash(inputBytes);
+                return Convert.ToHexString(hashBytes);
+            }
         }
     }
 }
